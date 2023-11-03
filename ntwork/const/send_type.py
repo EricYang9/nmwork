be3a00098eb816abc753304a92f2a3ef530af8d7
chazzcheung <149784564+chazzcheung@users.noreply.github.com>
2023-11-03 18:28:20 +0800

    Update send_type.py

--- ntwork/const/send_type.py
+++ ntwork/const/send_type.py
@@ -1,47 +1,255 @@
 # 获取自己的帐号信息
 MT_GET_SELF_MSG = 11035
+"""获取自己的帐号信息"""
 
 # 获取内部联系人列表
 MT_GET_INNER_CONTACTS_MSG = 11036
+"""获取内部联系人列表"""
 
 # 获取外部的联系人列表
 MT_GET_EXTERNAL_CONTACTS_MSG = 11037
+"""获取外部的联系人列表"""
 
 # 获取所有的群
 MT_GET_ROOMS_MSG = 11038
+"""获取所有的群"""
 
 # 获取指定联系人的详细信息
 MT_GET_CONTACT_DETAIL_MSG = 11039
+"""获取指定联系人的详细信息"""
 
-# 获取指定的群成员
+# 获取指定的群成员列表
 MT_GET_ROOM_MEMBERS_MSG = 11040
+"""获取指定的群成员列表"""
 
 # 发送文本消息
 MT_SEND_TEXT_MSG = 11029
+"""发送文本消息"""
 
 # 发送群@消息
 MT_SEND_ROOM_AT_MSG = 11069
+"""发送群@消息"""
 
 # 发送名片消息
 MT_SEND_CARD_MSG = 11034
+"""发送名片消息"""
 
 # 发送链接卡片消息
 MT_SEND_LINK_MSG = 11033
+"""发送链接卡片消息"""
 
 # 发送图片消息
 MT_SEND_IMAGE_MSG = 11030
+"""发送图片消息"""
 
 # 发送文件消息
 MT_SEND_FILE_MSG = 11031
+"""发送文件消息"""
 
 # 发送视频消息
 MT_SEND_VIDEO_MSG = 11067
+"""发送视频消息"""
+
+# 发送语音消息
+MT_SEND_VOICE_MSG = 11141
+"""发送语音消息"""
 
 # 发送gif消息
 MT_SEND_GIF_MSG = 11070
+"""发送gif消息"""
 
 # WXCDN下载
 MT_WXCDN_DOWNLOAD_MSG = 11171
+"""WXCDN下载"""
 
 # C2CCDN下载
 MT_C2CCDN_DOWNLOAD_MSG = 11170
+"""C2CCDN下载"""
+
+# CDN上传
+MT_CDN_UPLOAD_MSG = 11115
+"""CDN文件上传"""
+
+# 通过好友请求
+MT_ACCEPT_FRIEND_MSG = 11064
+"""通过好友请求"""
+
+# 发送小程序
+MT_SEND_MINIAPP_MSG = 11162
+"""发送小程序"""
+
+# 退出企业微信
+MT_EXIT = 11112
+"""退出企业微信"""
+
+# 添加好友进群
+MT_INVITE_TO_ROOM_MSG = 11060
+"""添加好友进群"""
+
+# 发送定位
+MT_SEND_POSITION_MSG = 11146
+"""发送定位"""
+
+# 发视频号
+MT_SEND_SPH_MSG = 11172
+"""发视频号"""
+
+# 发CDN视频
+MT_SEND_CDN_VIDEO_MSG = 11156
+"""发CDN视频"""
+
+# 创建空外部群聊
+MT_CREATE_EMPTY_ROOM_MSG = 11125
+"""创建空外部群聊"""
+
+# 添加名片好友
+MT_ADD_CARD_USER_MSG = 11121
+"""添加名片好友"""
+
+# 搜索微信/企微用户
+MT_SEARCH_USER_MSG = 11052
+"""搜索微信/企微用户"""
+
+# 添加搜索的微信用户
+MT_ADD_WECHAT_USER_MSG = 11053
+"""添加搜索的微信用户"""
+
+# 获取个人分享二维码
+MT_SHARE_QRCODE_MSG = 11122
+"""获取个人分享二维码"""
+
+# 删除联系人
+MT_DEL_USER_MSG = 11111
+"""删除联系人"""
+
+# 设置群名称
+MT_MOD_ROOM_NAME_MSG = 11059
+"""设置群名称"""
+
+# 移除群成员
+MT_DEL_ROOM_MEMBER_MSG = 11061
+"""移除群成员"""
+
+# 添加群成员好友
+MT_ADD_ROOM_MEMBER_MSG = 11071
+"""添加群成员好友"""
+
+# 发布群公告
+MT_MOD_ROOM_NOTICE_MSG = 11082
+"""发布群公告"""
+
+# 获取标签列表
+MT_DATA_TAG_LIST_MSG = 11084
+"""获取标签列表"""
+
+# 开启/关闭群邀请确认
+MT_CONFIG_ROOM_INVITE_MSG = 11089
+"""开启/关闭群邀请确认"""
+
+# 开启/关闭禁止修改群名
+MT_CONFIG_ROOM_MOD_NAME_MSG = 11108
+"""开启/关闭禁止修改群名"""
+
+# 获取群聊二维码 11079/11106
+MT_ROOM_QRCODE_MSG = 11106
+"""获取群聊二维码 11079/11106"""
+
+# 修改好友备注
+MT_MOD_USER_REMARK_MSG = 11054
+"""修改好友备注"""
+
+# 修改好友描述
+MT_MOD_USER_DESC_MSG = 11055
+"""修改好友描述"""
+
+# 修改客户手机号
+MT_MOD_USER_PHONE_MSG = 11056
+"""修改客户手机号"""
+
+# 修改客户公司名称
+MT_MOD_USER_COMPANY_MSG = 11057
+"""修改客户公司名称"""
+
+# 设置客户标签
+MT_TAG_USER_MSG = 11094
+"""设置客户标签"""
+
+# 获取指定客户的所有标签
+MT_DATA_USER_TAG_MSG = 11098
+"""获取指定客户的所有标签"""
+
+# 查询指定corp_id的公司名称和简称
+MT_SEARCH_USER_CORP_MSG = 11110
+"""查询指定corp_id的公司信息"""
+
+# 解散群聊
+MT_DISSOLVE_ROOM_MSG = 11130
+"""解散群聊"""
+
+# 获取群欢迎语列表
+MT_ROOM_WELCOME_MSG = 11131
+"""获取群欢迎语列表"""
+
+# 发布朋友圈
+MT_SNS_SEND_MSG = 11140
+"""发布朋友圈"""
+
+# 设置朋友圈签名
+MT_SNS_SIGN_MSG = 11143
+"""设置朋友圈签名"""
+
+# 查询指定corp_id的公司详细信息
+MT_SEARCH_CORP_MSG = 11148
+"""查询指定corp_id的公司详细信息"""
+
+# 获取朋友圈列表
+MT_SNS_LIST_MSG = 11168
+"""获取朋友圈列表"""
+
+# 获取被对方删除的客户列表
+MT_DEL_BY_USER_LIST_MSG = 11180
+"""获取被对方删除的客户列表"""
+
+# 获取当前账号公司信息列表
+MT_GET_ME_COMPANY_LIST_MSG = 11198
+"""获取当前账号公司信息列表"""
+
+# 添加群管理员
+MT_ADD_ROOM_ADMIN_MSG = 11118
+"""添加群管理员"""
+
+# 删除群管理员
+MT_DEL_ROOM_ADMIN_MSG = 11119
+"""删除群管理员"""
+
+# 退出指定群聊
+MT_EXIT_ROOM_MSG = 11105
+"""退出指定群聊"""
+
+# 发送文本信息(新版)
+MT_SEND_TEXT_V2_MSG = 11154
+"""发送文本信息(新版)"""
+
+# 发送CDN图片文件(新版)
+MT_SEND_IMG_V2_MSG = 11155
+"""发送CDN图片文件(新版)"""
+
+# 发送CDN视频文件(新版)
+MT_SEND_VIDEO_V2_MSG = 11157
+"""发送CDN视频文件(新版)"""
+
+# 发送自定义表情(新版)
+MT_SEND_GIF_V2_MSG = 11158
+"""转发表情包(新版)"""
+
+# 发送链接(新版)
+MT_SEND_LINK_V2_MSG = 11159
+"""发送链接(新版)"""
+
+# 发送群@消息(新版)
+MT_SEND_CHATROOM_AT_V2_MSG = 11160
+"""发送群@消息(新版)"""
+
+# 发送视频号直播信息(新版)
+MT_SEND_LIVE_MSG = 11196
+"""发送视频号直播信息"""
